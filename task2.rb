eb1c07c950826b208b46894321e43747099711fb
Python-is-a-snake <45516701+Python-is-a-snake@users.noreply.github.com>
2022-09-16 16:41:00 +0300

    Create task2.rb

---
+++ task2.rb
@@ -0,0 +1,20 @@
+=begin
+LAB 2 VAR 4 
+TASK 2
+Zaklevskyi Vladyslav IPS-32
+=end
+
+#Example from book p.33
+p1 = 6
+t1 = 15
+r1 = 10
+
+result1 = p1**r1 * (1 - 1/p1**t1)
+puts "Example result = #{result1}"
+
+#My task
+p = 5
+t = 8
+r = 0
+result = p**r * (1 - 1/p**t)
+puts "My result = #{result}"
